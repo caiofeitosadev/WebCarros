db80a7e764b733adbe6c6eb8158daad083bddd5d
caiofeitosadev <devcaioeduardo@gmail.com>
2026-03-04 01:13:55 -0300

    chore: add environment variables for firebase config

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbotLCgK-Na60h3zVQi2kOFxxrBvTJAnw',
-  authDomain: 'cursoapp-b0f7f.firebaseapp.com',
-  projectId: 'cursoapp-b0f7f',
-  storageBucket: 'cursoapp-b0f7f.firebasestorage.app',
-  messagingSenderId: '582879995682',
-  appId: '1:582879995682:web:7565c1a2fed3c5f538e77b',
-  measurementId: 'G-L64P4RM787',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
